1b99e2872ae5998b11ad2e8903ccb6ccdf039cde
domino <lolosiowo2>
2017-07-05 20:04:10 +0200

    session endpoint fix

--- src/main/java/com/yeabhunny/swizService/session/SessionController.java
+++ src/main/java/com/yeabhunny/swizService/session/SessionController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/session")
 public class SessionController {
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
